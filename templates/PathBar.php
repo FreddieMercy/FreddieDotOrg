bf3971c2d8df937e2428dd6657728f0edce44fa5
jzhang1 <junhao.zhang.freddie@gmail.com>
2016-08-27 02:22:36 -0700

    had done so far

---
+++ templates/PathBar.php
@@ -0,0 +1,11 @@
+<div class="PathBar sidebar" align="center">
+	<nav>
+		<ul>
+							
+		</ul>
+
+
+	</nav>
+</div>
+
+<script type="text/javascript" src="/static/js/PathBar.js"></script> 
---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+<footer align="center" >
+
+			    <p>&copy;<?php echo date("Y"); ?> 大连峻达医药有限公司, 其他的我还没想好要写什么</p>
+
+</footer>
