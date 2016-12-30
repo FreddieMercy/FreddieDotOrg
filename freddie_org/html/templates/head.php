e2c8ce29c8ada52260b0741b9fd002093a3af107
Junhao <junhao.zhang.freddie@gmail.com>
2016-12-29 18:34:57 -0800

    init4

---
+++ freddie_org/html/templates/head.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="/static/css/mystyle.css">	
+<script src="/static/js/Plugin/jQuery.js"></script> 	
+<script src="/static/js/Plugin/headroom.js"></script> 
+<script src="/static/js/Plugin/jQuery.headroom.js"></script> 
+<script type="text/javascript" src="/static/js/adjust.js"></script> 		
+<title></title>
+  
