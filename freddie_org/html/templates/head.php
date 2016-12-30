25dd9c4c1cd457418ee796d4f16253c2d603b13d
Junhao <junhao.zhang.freddie@gmail.com>
2016-12-29 18:39:14 -0800

    test

--- freddie_org/html/templates/head.php
+++
@@ -1,8 +0,0 @@
-<meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="/static/css/mystyle.css">	
-<script src="/static/js/Plugin/jQuery.js"></script> 	
-<script src="/static/js/Plugin/headroom.js"></script> 
-<script src="/static/js/Plugin/jQuery.headroom.js"></script> 
-<script type="text/javascript" src="/static/js/adjust.js"></script> 		
-<title></title>
-  
