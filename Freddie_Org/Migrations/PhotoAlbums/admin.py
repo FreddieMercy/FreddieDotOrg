e0b3d6dd0711a54ae1deb57baf0b609a198d3219
FreddieMercy <junhao.zhang.freddie@gmail.com>
2021-04-06 10:20:53 -0700

    Empty

--- Freddie_Org/Migrations/PhotoAlbums/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import LeftAndRightArrows, PhotoAlbumModel
-
-class PhotoAlbumsAdmin(admin.ModelAdmin):
-    list_filter = ['Date_Added']
-
-admin.site.register(LeftAndRightArrows, PhotoAlbumsAdmin)
-admin.site.register(PhotoAlbumModel)
